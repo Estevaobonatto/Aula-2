5fbc402a0282efcd2751b2ac285272f2119124ec
Estevão Bonatto <estevaobonatto002@gmail.com>
2024-05-29 21:25:00 -0300

    p2 client

--- UDPClient.java
+++ UDPClient.java
@@ -14,6 +14,16 @@ public class UDPClient {
             byte[] sendData = message.getBytes();
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverIpAddress, serverPort);
             clientSocket.send(sendPacket);
+
+            //segunda parte
+
+            byte[] receivedData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receivedData, receivedData.length);
+            clientSocket.receive(receivePacket);
+
+            String responseMessage = new String(receivePacket.getData());
+            System.out.println("Mensagem de resposta: " + responseMessage);
+
             clientSocket.close();
         } catch (Exception e) {
             System.out.println(e.toString());
